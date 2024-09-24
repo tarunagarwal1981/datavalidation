c411d44c5e29bcb502da4f9b7eebdd00d107e2ad
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-24 21:38:05 +0530

    Update database.py

--- database.py
+++ database.py
@@ -16,36 +16,3 @@ def get_db_engine():
     db_url = f"postgresql+psycopg2://{DB_CONFIG['user']}:{encoded_password}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['database']}"
     engine = create_engine(db_url)
     return engine
-
-def fetch_vessel_performance_data(engine):
-    query = """
-    SELECT vps.*, vp.vessel_type
-    FROM vessel_performance_summary vps
-    LEFT JOIN vessel_particulars vp ON vps.vessel_name = vp.vessel_name
-    WHERE vps.reportdate >= %s;
-    """
-    three_months_ago = datetime.now() - timedelta(days=90)
-    df = pd.read_sql_query(query, engine, params=(three_months_ago,))
-    return df
-
-def fetch_vessel_coefficients(engine):
-    query = """
-    SELECT *
-    FROM vessel_performance_coefficients;
-    """
-    return pd.read_sql_query(query, engine)
-
-def fetch_hull_performance_data(engine):
-    query = """
-    SELECT vessel_name, hull_rough_power_loss_pct_ed
-    FROM hull_performance_six_months;
-    """
-    return pd.read_sql_query(query, engine)
-
-def fetch_mcr_data(engine):
-    query = """
-    SELECT "Vessel_Name", 
-           CAST(NULLIF("ME_1_MCR_kW", '') AS FLOAT) AS "ME_1_MCR_kW"
-    FROM machinery_particulars;
-    """
-    return pd.read_sql_query(query, engine)
