e0eb95c9903a08d5b5900a2e895d68f27ffc2dea
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-24 11:42:13 +0530

    Update __init__.py

--- validators/__init__.py
+++ validators/__init__.py
@@ -1,7 +1,7 @@
 from .me_consumption_validation import validate_me_consumption
 from .ae_consumption_validation import validate_ae_consumption
-from config import COLUMN_NAMES
-from utils.validation_utils import calculate_historical_average
+from config import COLUMN_NAMES, VALIDATION_THRESHOLDS
+from utils.validation_utils import calculate_historical_average, calculate_expected_consumption
 
 def run_all_validations(df, coefficients_df, hull_performance_df):
     validation_results = []
@@ -20,14 +20,18 @@ def run_all_validations(df, coefficients_df, hull_performance_df):
             
             # ME consumption validation
             me_historical_data = {'avg_me_consumption': historical_me_data.get(vessel_name)}
-            failure_reasons.extend(validate_me_consumption(row, vessel_type, me_historical_data))
+            failure_reasons.extend(validate_me_consumption(
+                row, 
+                vessel_type, 
+                me_historical_data, 
+                vessel_coefficients, 
+                hull_performance_factor
+            ))
             
             # AE consumption validation
             ae_historical_data = {'avg_ae_consumption': historical_ae_data.get(vessel_name)}
             failure_reasons.extend(validate_ae_consumption(row, ae_historical_data))
             
-            # Add more validations here as you create them...
-
             if failure_reasons:
                 validation_results.append({
                     'Vessel Name': vessel_name,
