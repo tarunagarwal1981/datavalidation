1d2b190fe869c6483cb32fbd2a92fb4c2517070c
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-10-05 08:36:42 +0530

    Update advanced_validation.py

--- validators/advanced_validation.py
+++ validators/advanced_validation.py
@@ -73,8 +73,6 @@ def detect_anomalies(df):
         st.warning("Not enough data for anomaly detection.")
         return pd.DataFrame(columns=[COLUMN_NAMES['VESSEL_NAME'], COLUMN_NAMES['REPORT_DATE'], 'Discrepancy'])
 
-    anomalies = pd.DataFrame()  # Initialize an empty DataFrame for anomalies
-
     try:
         iso_forest = IsolationForest(contamination=0.1, random_state=42)
         iso_forest_anomalies = iso_forest.fit_predict(df[features])
@@ -83,7 +81,7 @@ def detect_anomalies(df):
         lof_anomalies = lof.fit_predict(df[features])
 
         combined_anomalies = np.logical_and(iso_forest_anomalies == -1, lof_anomalies == -1)
-        anomalies = df[combined_anomalies]  # Update the 'anomalies' DataFrame if anomalies are detected
+        anomalies = df[combined_anomalies]
 
         if anomalies.empty:
             st.info("No anomalies detected.")
